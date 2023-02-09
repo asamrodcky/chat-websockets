76237fc8aa49044fc95614ec2388092893e54140
Samuel Rodriguez <samrodriguez826@gmail.com>
2023-02-09 13:26:50 -0500

    converted hardcoded messages to objects

--- server.js
+++ server.js
@@ -11,22 +11,24 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'ChatCord Bot';
+
 // Run when client connects
 io.on('connection', socket =>{
    // Welcome current user
-   socket.emit('message', 'Welcome to ChatCord!');
+   socket.emit('message', formatMessage(botName,'Welcome to ChatCord!'));
 
    // Broadcast when a user connects
-   socket.broadcast.emit('message', 'A user has joined the chat');
+   socket.broadcast.emit('message', formatMessage(botName,'A user has joined the chat'));
 
    // Runs when client disconnects
    socket.on('disconnect', () => {
-    io.emit('message', 'A user has left the chat');
+    io.emit('message', formatMessage(botName,'A user has left the chat'));
    });
 
    // Listen for chatMessage
    socket.on('chatMessage', msg => {
-    io.emit('message', msg);
+    io.emit('message', formatMessage('USER', msg));
    })
 });
 
